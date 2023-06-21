8b13b0db54c92bb7dc236115ccc3fe9a9fb361e1
karol.jarosinski <karol.jarosinski91@gmail.com>
2023-06-21 13:23:22 +0200

    next js

--- js/app.js
+++ js/app.js
@@ -4,17 +4,35 @@ const options = dropdownOptions.querySelectorAll('li');
 const selectedOption = dropdownSelect.querySelector('.select__input');
 const calcSummary = document.querySelector('.calc__summary');
 const calcPackage = calcSummary.querySelector('[data-id="package"]');
-const itemPrice = calcPackage.querySelector('.item__price');
+const packagePrice = calcPackage.querySelector('.item__price');
 const packagePrices = document.querySelectorAll('.type_price')
+const calcCheckboxes = document.querySelectorAll('.calc .checkbox');
+// const calcAccounting = calcSummary.querySelector('[data-id="accounting"]');
+// const accPrice = calcAccounting.querySelector('.item__price');
 
 dropdownSelect.addEventListener('click', showDropdownOptions);
-options.forEach((el) => {
-    el.addEventListener('click', handleSelection);
-})
+options.forEach((el) =>
+    el.addEventListener('click', handleSelection));
+calcCheckboxes.forEach(el => el.addEventListener('click', handleCheckboxClick));
+
+function handleCheckboxClick(event) {
+    if (event.target.tagName === 'LABEL' || event.target.tagName === 'SPAN') {
+        if (event.currentTarget.textContent.includes('Accounting')) {
+            // event.currentTarget.querySelector('input').setAttribute('checked', '');
+            console.log(event.currentTarget.querySelector('input').checked)
+            // event.currentTarget.querySelector('input').checked? calcAccounting.style.display = 'flex' : calcAccounting.style.display = 'none';
+            // accPrice.innerText = '$35'
+        }
+        if (event.currentTarget.textContent.includes('Rental of a payment terminal')) {
+            console.log('dupa2')
+        }
+    }
+}
 
 function showDropdownOptions(event) {
     if (dropdownOptions.style.display === 'none' || dropdownOptions.getAttribute('style') === null) {
-        dropdownOptions.style.display = 'block';
+        dropdownOptions.style.display = 'flex';
+        dropdownOptions.style.flexDirection = 'column'
     } else {
         dropdownOptions.style.display = 'none';
     }
@@ -27,15 +45,15 @@ function handleSelection(event) {
     calcPackage.querySelector('.item__calc').innerText = event.currentTarget.innerHTML;
     switch (selection) {
         case 'Basic':
-            itemPrice.innerText = packagePrice;
+            packagePrice.innerText = packagePrice;
             break;
         case 'Professional':
-            itemPrice.innerText = packagePrice;
+            packagePrice.innerText = packagePrice;
             break;
         case 'Premium':
-            itemPrice.innerText = packagePrice;
+            packagePrice.innerText = packagePrice;
     }
-    calcPackage.style.display = 'block';
+    calcPackage.style.display = 'flex';
     dropdownOptions.style.display = 'none';
 }
 function getPackagePrice(packages, packageName) {
