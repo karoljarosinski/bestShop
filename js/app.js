4ca77e183e80469aa3e79d753f40e537d4a99772
karol.jarosinski <karol.jarosinski91@gmail.com>
2023-06-21 15:27:07 +0200

    calc summary

--- js/app.js
+++ js/app.js
@@ -3,36 +3,45 @@ const dropdownOptions = dropdownSelect.querySelector('.select__dropdown');
 const options = dropdownOptions.querySelectorAll('li');
 const selectedOption = dropdownSelect.querySelector('.select__input');
 const calcSummary = document.querySelector('.calc__summary');
-const calcPackage = calcSummary.querySelector('[data-id="package"]');
-const packagePrice = calcPackage.querySelector('.item__price');
-const packagePrices = document.querySelectorAll('.type_price')
-const calcCheckboxes = document.querySelectorAll('.calc .checkbox');
-// const calcAccounting = calcSummary.querySelector('[data-id="accounting"]');
-// const accPrice = calcAccounting.querySelector('.item__price');
+const productsCalc = calcSummary.querySelector('[data-id="products"]');
+const ordersCalc = calcSummary.querySelector('[data-id="orders"]');
+const packageCalc = calcSummary.querySelector('[data-id="package"]');
+const packagesPriceInfo = document.querySelectorAll('.type_price')
+const calcCheckboxes = document.querySelectorAll('.calc input[type="checkbox"]');
+const accountingCalc = calcSummary.querySelector('[data-id="accounting"]');
+const rentalCalc = calcSummary.querySelector('[data-id="terminal"]');
+const inputsNumber = document.querySelectorAll('.form__input');
+
+inputsNumber.forEach(input => input.addEventListener('input', handleTyping));
 
 dropdownSelect.addEventListener('click', showDropdownOptions);
-options.forEach((el) =>
-    el.addEventListener('click', handleSelection));
+
+options.forEach(el => el.addEventListener('click', handleSelection));
+
 calcCheckboxes.forEach(el => el.addEventListener('click', handleCheckboxClick));
 
-function handleCheckboxClick(event) {
-    if (event.target.tagName === 'LABEL' || event.target.tagName === 'SPAN') {
-        if (event.currentTarget.textContent.includes('Accounting')) {
-            // event.currentTarget.querySelector('input').setAttribute('checked', '');
-            console.log(event.currentTarget.querySelector('input').checked)
-            // event.currentTarget.querySelector('input').checked? calcAccounting.style.display = 'flex' : calcAccounting.style.display = 'none';
-            // accPrice.innerText = '$35'
-        }
-        if (event.currentTarget.textContent.includes('Rental of a payment terminal')) {
-            console.log('dupa2')
-        }
+function handleTyping(event) {
+    if (event.target.id === 'products') {
+        productsCalc.style.display = 'flex';
+    } else {
+        ordersCalc.style.display = 'flex';
     }
 }
 
-function showDropdownOptions(event) {
+function handleCheckboxClick(event) {
+    if (event.target.id === 'accounting') {
+        event.target.checked ? accountingCalc.style.display = 'flex' : accountingCalc.style.display = 'none';
+        accountingCalc.querySelector('.item__price').innerText = '$35';
+    }
+    if (event.target.id === 'rental') {
+        event.target.checked ? rentalCalc.style.display = 'flex' : rentalCalc.style.display = 'none';
+        rentalCalc.querySelector('.item__price').innerText = '$5';
+    }
+}
+
+function showDropdownOptions() {
     if (dropdownOptions.style.display === 'none' || dropdownOptions.getAttribute('style') === null) {
-        dropdownOptions.style.display = 'flex';
-        dropdownOptions.style.flexDirection = 'column'
+        dropdownOptions.style.display = 'block';
     } else {
         dropdownOptions.style.display = 'none';
     }
@@ -40,22 +49,15 @@ function showDropdownOptions(event) {
 
 function handleSelection(event) {
     event.stopPropagation();
-    const selection = selectedOption.innerText = event.currentTarget.innerHTML;
-    const packagePrice = getPackagePrice(packagePrices, selection);
-    calcPackage.querySelector('.item__calc').innerText = event.currentTarget.innerHTML;
-    switch (selection) {
-        case 'Basic':
-            packagePrice.innerText = packagePrice;
-            break;
-        case 'Professional':
-            packagePrice.innerText = packagePrice;
-            break;
-        case 'Premium':
-            packagePrice.innerText = packagePrice;
-    }
-    calcPackage.style.display = 'flex';
+    const selection = event.currentTarget.innerHTML;
+    selectedOption.innerText = selection;
+    const selectedPrice = getPackagePrice(packagesPriceInfo, selection);
+    packageCalc.querySelector('.item__calc').innerText = event.currentTarget.innerHTML;
+    packageCalc.querySelector('.item__price').innerText = selectedPrice;
+    packageCalc.style.display = 'flex';
     dropdownOptions.style.display = 'none';
 }
+
 function getPackagePrice(packages, packageName) {
     return Array.from(packages).find(el => el.textContent.includes(packageName)).querySelector('p').innerHTML;
 }
